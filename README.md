b4d2015bf41a543c702bb9ff65728954b0f3b181
Sergii Mertsalov & Katharine Chen <student@epicodus.com>
2014-07-16 12:06:20 -0700

    Refactored scripts.js with new README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 <h1>Triangle Tracker</h1>
+
+Written by Sergii and Katharine Chen.
+
+If a user enters an invalid triangle, pop up an alert when they try to submit the form.
+
+You can reference your code from last week on figuring out what type of triangle three sides form. Create a method on your triangle objects called type() that returns the type of triangle it is. Hint: the method shouldn't take any arguments, as it can reference its own sides using the this keyword.
+
+Don't worry about writing tests; we'll get to that in the next lesson.
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 var triangle = {
-	side1: 0,
-	side2: 0,
-	side3: 0,
 	type: function() {
 		var sideArray = [this.side1, this.side2, this.side3].sort();
 
@@ -22,10 +19,6 @@ var triangle = {
 
 $(document).ready(function(){
 
-	var equilateralObject = { values:[] };
-	var isoscelesObject = { values: [] };
-	var scaleneObject = { values: [] };
-
 	$("form.triangles").submit(function(event){
 	event.preventDefault();
 
@@ -36,13 +29,10 @@ $(document).ready(function(){
 	if (triangle.type() === false) {
 		alert("Please enter a valid triangle");
 	} else if (triangle.type() === "equilateral") {
-			equilateralObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
 			$("div#equilateral").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
 	} else if (triangle.type() === "isosceles") {
-			isoscelesObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
 			$("div#isosceles").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
 	} else {
-			scaleneObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
 			$("div#scalene").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
 	}
 
