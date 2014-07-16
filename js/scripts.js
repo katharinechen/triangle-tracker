f0561fa56c8afbc5ca6ebe23135b736124808987
Sergii Mertsalov & Katharine Chen <student@epicodus.com>
2014-07-16 11:51:19 -0700

    stored logic in object

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,53 @@
-var triangle = function(side1, side2, side3) {
+var triangle = {
+	side1: 0,
+	side2: 0,
+	side3: 0,
+	type: function() {
+		var sideArray = [this.side1, this.side2, this.side3].sort();
 
-	var sideArray = [side1, side2, side3].sort();
-
- if ((sideArray[0] + sideArray[1]) <= sideArray[2]) {
-		return false;
-	} else {
-			if ((sideArray[0] === sideArray[1]) && (sideArray[1] === sideArray[2])) {
-				return "equilateral";
-			} else if ((sideArray[0] === sideArray[1]) && (sideArray[0] + sideArray[1] > sideArray[2])) {
-				return "isosceles";
-			} else {
-				return "scalene";
-			}
+	 	if ((sideArray[0] + sideArray[1]) <= sideArray[2]) {
+			return false;
+		} else {
+				if ((sideArray[0] === sideArray[1]) && (sideArray[1] === sideArray[2])) {
+					return "equilateral";
+				} else if ((sideArray[0] === sideArray[1]) && (sideArray[0] + sideArray[1] > sideArray[2])) {
+					return "isosceles";
+				} else {
+					return "scalene";
+				}
+		}
 	}
 };
 
+
+$(document).ready(function(){
+
+	var equilateralObject = { values:[] };
+	var isoscelesObject = { values: [] };
+	var scaleneObject = { values: [] };
+
+	$("form.triangles").submit(function(event){
+	event.preventDefault();
+
+	triangle.side1 = parseInt($("input#side1").val());
+	triangle.side2 = parseInt($("input#side2").val());
+	triangle.side3 = parseInt($("input#side3").val());
+
+	if (triangle.type() === false) {
+		alert("Please enter a valid triangle");
+	} else if (triangle.type() === "equilateral") {
+			equilateralObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
+			$("div#equilateral").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
+	} else if (triangle.type() === "isosceles") {
+			isoscelesObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
+			$("div#isosceles").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
+	} else {
+			scaleneObject.values.push([triangle.side1, triangle.side2, triangle.side3]);
+			$("div#scalene").append("<li>"+ triangle.side1 + "," + triangle.side2 + "," + triangle.side3 + " </li>");
+	}
+
+	});
+});
 
 
 
